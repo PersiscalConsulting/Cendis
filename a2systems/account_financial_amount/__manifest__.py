53a86c79835d51910d084606b3a565abec441592
Ivan Arriola <admin@autodidactati.com>
2025-04-01 12:50:10 -0300

    [UPD] Pre V17

--- a2systems/account_financial_amount/__manifest__.py
+++ a2systems/account_financial_amount/__manifest__.py
@@ -1,19 +1,18 @@
 {
     "name": "Accounting Financial Amounts",
-    "version": "13.0.1.0.0",
-    "author": "ADHOC SA",
+    "version": "17.0.1.0.0",
+    "author": "A2 Systems, ADHOC SA",
     "license": "AGPL-3",
     "category": "Accounting",
     "depends": [
         "account",
     ],
     "data": [
-        'security/account_debt_management_security.xml',
-        'wizard/res_config_settings_views.xml',
+        "views/account_view.xml"
     ],
     "demo": [
     ],
     'images': [
     ],
-    'installable': False,
+    'installable': True,
 }
