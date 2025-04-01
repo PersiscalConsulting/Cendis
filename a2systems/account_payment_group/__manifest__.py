5ebafcbe460c78c21d9d261300f0f3d0d232ebb7
Ivan Arriola <admin@autodidactati.com>
2025-04-01 13:01:23 -0300

    [UPD] Pre V17

--- a2systems/account_payment_group/__manifest__.py
+++ a2systems/account_payment_group/__manifest__.py
@@ -16,7 +16,8 @@
         "account_financial_amount",
         # for fixes related to domains and company_id field on form view
         "account_payment_fix",
-        "l10n_latam_check"
+        "l10n_latam_check",
+        "l10n_ar_edi_ux",
     ],
     "data": [
         'security/security.xml',
