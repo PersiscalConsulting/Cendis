ee882ad99b3fb92006ea2e474a38aef73944d2a2
Ivan Arriola <admin@autodidactati.com>
2025-05-17 19:23:54 -0300

    [ADD] Agregado temportal account_payment_group

--- account_payment_group/__manifest__.py
+++ account_payment_group/__manifest__.py
@@ -15,7 +15,7 @@
     },
     "depends": [
         "account_ux",
-        "l10n_latam_account_sequence",
+        #"l10n_latam_account_sequence",
     ],
     "data": [
         'security/security.xml',
