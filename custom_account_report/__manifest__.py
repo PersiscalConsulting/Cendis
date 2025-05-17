f46bb0a2e1a53427f1417991ba74afe353c270c3
Ivan Arriola <admin@autodidactati.com>
2025-05-17 18:59:28 -0300

    [DEL] submodules adhoc

--- custom_account_report/__manifest__.py
+++ custom_account_report/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Custom report account',
-    'version': "16.0.1.1.0",
+    'version': "17.0.1.1.0",
     'author': 'Persiscal',
     'website': 'www.Persiscal.com',
     'license': 'AGPL-3',
