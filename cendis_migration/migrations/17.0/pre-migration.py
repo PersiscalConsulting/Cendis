1272eee70caae167b5a45cad64fb91d4d1e55fbe
Ivan Arriola <admin@autodidactati.com>
2025-05-19 16:00:18 -0300

    [ADD] script migration

---
+++ cendis_migration/migrations/17.0/pre-migration.py
@@ -0,0 +1,12 @@
+import logging
+from odoo.upgrade import util
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    _logger.info("Cambiamos nombre tecnico de modulo account_payment_group por account_payment_pro 1")
+    env = util.env(cr)
+    
+    util.rename_module(cr, "account_payment_group", "account_payment_pro")
+    _logger.info("Cambiamos nombre tecnico de modulo account_payment_group por account_payment_pro 2")
