c98d4ce0be79746811032cdbd02252b7ce268ff1
Ivan Arriola <admin@autodidactati.com>
2025-05-19 16:20:30 -0300

    [UPD] script migration

--- ingadhoc/odoo-argentina/l10n_ar_ux/migrations/17.0.1.1.0/pre-migration.py
+++ ingadhoc/odoo-argentina/l10n_ar_ux/migrations/17.0.1.1.0/pre-migration.py
@@ -8,5 +8,5 @@ def migrate(cr, version):
     _logger.info("Cambiamos nombre tecnico de modulo account_payment_group por account_payment_pro 1")
     env = util.env(cr)
     
-    util.rename_module(cr, "account_payment_group", "account_payment_pro")
+    util.merge_module(cr, "account_payment_group", "account_payment_pro", update_dependers=True)
     _logger.info("Cambiamos nombre tecnico de modulo account_payment_group por account_payment_pro 2")
