ed6045766724ed4de1ecc8cb4cf9590aa6348151
Ivan Arriola <admin@autodidactati.com>
2025-05-23 15:42:14 -0300

    [UPD] script migration

--- ingadhoc/odoo-argentina/l10n_ar_ux/migrations/17.0.1.1.0/pre-migration.py
+++ ingadhoc/odoo-argentina/l10n_ar_ux/migrations/17.0.1.1.0/pre-migration.py
@@ -11,7 +11,8 @@ def migrate(cr, version):
     util.merge_module(cr, "account_payment_group", "account_payment_pro", update_dependers=True)
     util.merge_module(cr, "account_withholding", "l10n_ar_account_withholding", update_dependers=True)
     util.merge_module(cr, "account_withholding_automatic", "l10n_ar_withholding_ux", update_dependers=True)
-    _logger.info("Cambiamos nombre tecnico de modulo account_payment_group por account_payment_pro 2")
+    _logger.info("Removemos el modelo account_payment_group")
+    util.merge_module(cr, "account.payment.group", drop_table=True, ignore_m2m=())
 
     """
     Fix duplicated account.move name/journal_id combinations to allow applying
