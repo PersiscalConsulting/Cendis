2509b4086304a45101dd4709c2879e88c02b7904
LautaChoque <lchoque@cendis.com.ar>
2026-06-08 10:15:59 -0300

    Fix: Correccion de correccion de remitos

---
+++ custom/custom_stock_picking_enhancement_correccion/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Cendis - Correcciones Combinación Remitos',
+    'version': '18.0.1.0.0',
+    'category': 'Inventory',
+    'summary': 'Corrección propia para arrastrar destino_id en remitos combinados',
+    'depends': ['stock', 'custom_stock_picking_enhancement'],
+    'auto_install': True,
+    'installable': True,
+    'license': 'LGPL-3',
+}
